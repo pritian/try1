15784913dc2caf3d9d0a6f31640ada3922eb1249
pritian <pritishukla413@gmail.com>
2021-07-15 20:54:17 +0530

    logicloop code pushed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  profielData="How Did Van Gogh's Turbulent Mind Depict One of the Most Comples Concepts in Physics?";
+  like = 20;
+  chat = 24;
+  view = 30;
+  profilePera ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus sapiente, dolorum consectetur est accusamus quis? Quae enim, aperiam aut asperiores dolores"
+  constructor() { }
+
+  ngOnInit(): void {
+
+
+  }
+
+
+
+}
